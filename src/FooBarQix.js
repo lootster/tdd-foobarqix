221b9c9433cce014cb66660fd5bf62271d2edc92
Sheldon <sheldon.lim@online-pajak.com>
2019-02-15 10:23:03 +0800

    Write test cases for testing number 1, 3, 5 and 7

--- src/FooBarQix.js
+++ src/FooBarQix.js
@@ -1,6 +1,12 @@
+const MESSAGE_FOO = "Foo";
+const MESSAGE_BAR = "Bar";
+const MESSAGE_QIX = "Qix";
+
 class FooBarQix {
   testNumber(number) {
-    if (number % 3 === 0) return "Foo";
+    if (number % 3 === 0 || number === 3) return MESSAGE_FOO;
+    if (number % 5 === 0 || number === 5) return MESSAGE_BAR;
+    if (number % 7 === 0 || number === 7) return MESSAGE_QIX;
     return number;
   }
 }
--- test/fooBarQix.test.js
+++ test/fooBarQix.test.js
@@ -6,8 +6,20 @@ test("should return 1 if input is 1", () => {
   expect(message).toBe(1);
 });
 
-test('should return "foo" if the number is divisible by 3', () => {
+test('should return "Foo" if the number is divisible by 3', () => {
   let bot = new FooBarQix();
   let message = bot.testNumber(3);
   expect(message).toBe("Foo");
+});
+
+test('should return "Bar" if the number is divisible by 5', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(5);
+  expect(message).toBe("Bar");
+});
+
+test('should return "Qix" if the number is divisible by 7', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(7);
+  expect(message).toBe("Qix");
 });
