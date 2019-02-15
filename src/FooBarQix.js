b8c9f793566cedbc9ee87377856687ce2f978bd6
Sheldon <sheldon.lim@online-pajak.com>
2019-02-15 15:01:18 +0800

    Completed all test cases

--- src/FooBarQix.js
+++ src/FooBarQix.js
@@ -1,14 +1,50 @@
 const MESSAGE_FOO = "Foo";
 const MESSAGE_BAR = "Bar";
 const MESSAGE_QIX = "Qix";
+const THREE = 3;
+const FIVE = 5;
+const SEVEN = 7;
 
 class FooBarQix {
   testNumber(number) {
-    if (number % 3 === 0 || number === 3) return MESSAGE_FOO;
-    if (number % 5 === 0 || number === 5) return MESSAGE_BAR;
-    if (number % 7 === 0 || number === 7) return MESSAGE_QIX;
-    return number;
+    let output = "";
+    let numberToString = number.toString();
+
+    if (isDivisibleBy3(number)) output += MESSAGE_FOO;
+    if (isDivisibleBy5(number)) output += MESSAGE_BAR;
+    if (isDivisibleBy7(number)) output += MESSAGE_QIX;
+    if (numberToString.length > 1) {
+      return containFooBarQiz(numberToString, output);
+    }
+    return output ? output : number;
   }
 }
 
 module.exports = FooBarQix;
+
+function containFooBarQiz(numberToString, output) {
+  for (let i = 0; i < numberToString.length; i++) {
+    if (numberToString.charAt(i).includes(THREE.toString())) {
+      output += MESSAGE_FOO;
+    }
+    if (numberToString.charAt(i).includes(FIVE.toString())) {
+      output += MESSAGE_BAR;
+    }
+    if (numberToString.charAt(i).includes(SEVEN.toString())) {
+      output += MESSAGE_QIX;
+    }
+  }
+  return output;
+}
+
+function isDivisibleBy3(number) {
+  return number % THREE === 0;
+}
+
+function isDivisibleBy5(number) {
+  return number % FIVE === 0;
+}
+
+function isDivisibleBy7(number) {
+  return number % SEVEN === 0;
+}
--- test/fooBarQix.test.js
+++ test/fooBarQix.test.js
@@ -23,3 +23,47 @@ test('should return "Qix" if the number is divisible by 7', () => {
   let message = bot.testNumber(7);
   expect(message).toBe("Qix");
 });
+
+test('should return "FooBar" if the number is 51', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(51);
+  expect(message).toBe("FooBar");
+});
+
+test('should return "BarFoo" if the number is 53', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(53);
+  expect(message).toBe("BarFoo");
+});
+
+test('should return "FooQix" if the number is 21', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(21);
+  expect(message).toBe("FooQix");
+});
+
+test('should return "Foo" if the number is 13', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(13);
+  expect(message).toBe("Foo");
+});
+
+test('should return "FooBarBar" if the number is 15', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(15);
+  expect(message).toBe("FooBarBar");
+});
+
+test('should return "FooFooFoo" if the number is 33', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(33);
+  expect(message).toBe("FooFooFoo");
+});
+
+test('should return "FooBar" if the number is 51', () => {
+  let bot = new FooBarQix();
+  let message = bot.testNumber(27);
+  expect(message).toBe("FooQix");
+});
+
+
